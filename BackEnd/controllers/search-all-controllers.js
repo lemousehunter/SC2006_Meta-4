c2183946cdd2cb3e199ac1ffa3230b15428e6fed
Bernard Chiang <bczh2000@gmail.com>
2023-04-04 17:53:43 +0800

    controller for search all added

---
+++ BackEnd/controllers/search-all-controllers.js
@@ -0,0 +1,42 @@
+const { Category } = require("../models/category");
+const mongoose = require("mongoose");
+const HttpError = require("../models/http-error");
+const { User } = require("../models/user");
+const { Post } = require("../models/post");
+
+// Search both post and user via name (Case sensitive)
+const search =  async (req, res, next) => {
+    const { name } = req.params;
+    const { category, listedBy } = req.query;
+
+    let postData, userData;
+    try {
+        const filter = new RegExp(name, 'i');
+        const categoryFilter = category ? mongoose.Types.ObjectId(category) : undefined;
+        const listedByFilter = listedBy ? { $regex: listedBy, $options: 'i' } : undefined;        
+
+      userData = await User.find({
+        $or: [{ name: { $regex: req.params.name } }],
+      }).select({ name: 1, email: 1, phone: 1, posts: 1 });
+      postData =await Post.find({
+        $or: [
+          { itemName: filter },
+          { category: categoryFilter },
+          { listedBy: listedByFilter }
+        ]
+      })
+      .populate("category")
+      .populate({ path: "listedBy", select: { name: 1, phone: 1 } });
+    } catch (err) {
+      const error = new HttpError(
+        "Could not find the specified user given the name.",
+        500
+      );
+      return next(error);
+    }
+
+    const responseData = {userData, postData};
+    res.status(201).send(responseData);
+  };
+  
+  exports.search = search;
--- BackEnd/routes/search-all.js
+++ BackEnd/routes/search-all.js
@@ -1,45 +1,8 @@
-const { Post } = require("../models/post");
-const express = require("express");
-const { Category } = require("../models/category");
+const express = require('express');
+
+const searchAllControllers = require('../controllers/search-all-controllers');
 const router = express.Router();
-const mongoose = require("mongoose");
-const multer = require("multer");
-const HttpError = require("../models/http-error");
-const { User } = require("../models/user");
 
-// Search both post and user via name (Case sensitive)
-router.get(`/:name`, async (req, res, next) => {
-    const { name } = req.params;
-    const { category, listedBy } = req.query;
+router.get("/:name", searchAllControllers.search);
 
-    let postData, userData;
-    try {
-        const filter = new RegExp(name, 'i');
-        const categoryFilter = category ? mongoose.Types.ObjectId(category) : undefined;
-        const listedByFilter = listedBy ? { $regex: listedBy, $options: 'i' } : undefined;        
-
-      userData = await User.find({
-        $or: [{ name: { $regex: req.params.name } }],
-      }).select({ name: 1, email: 1, phone: 1, posts: 1 });
-      postData =await Post.find({
-        $or: [
-          { itemName: filter },
-          { category: categoryFilter },
-          { listedBy: listedByFilter }
-        ]
-      })
-      .populate("category")
-      .populate({ path: "listedBy", select: { name: 1, phone: 1 } });
-    } catch (err) {
-      const error = new HttpError(
-        "Could not find the specified user given the name.",
-        500
-      );
-      return next(error);
-    }
-
-    const responseData = {userData, postData};
-    res.status(201).send(responseData);
-  });
-  
-  module.exports = router;
+module.exports = router; 
