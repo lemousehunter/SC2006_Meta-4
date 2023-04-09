d87850341f0b279c4739c4d52c0aa046c7e317bc
pratelagger <xxyuanlinxx@hotmail.com>
2023-04-09 21:44:26 +0800

    Update NCardRectangle.js

--- FrontEnd/LostAndFound/components/reusable/Neuromorphic/Cards/NCardRectangle.js
+++ FrontEnd/LostAndFound/components/reusable/Neuromorphic/Cards/NCardRectangle.js
@@ -1,9 +1,19 @@
 import NCardBase from './NCardBase';
 
+/**
+ * A rectangular card component with customizable border radius.
+ * Extends the NCardBase class.
+ */
 export default class NCardRectangle extends NCardBase {
+  /**
+   * Creates a new NCardRectangle instance.
+   * @param {Object} props - The props for the component.
+   * @param {number} [props.borderRadius=20] - The border radius of the card. Default value is 20.
+   */
   constructor(props) {
     super(props);
     this.borderRadius = this.borderRadius == null ? 20 : this.borderRadius;
     this.createStylesheet(); // update stylesheet with borderRadius
   }
 }
+  
