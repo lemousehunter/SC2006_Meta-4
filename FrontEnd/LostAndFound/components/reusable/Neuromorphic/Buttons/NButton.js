aa8567419ccccd8049f5027d74670d38e7b5b823
pratelagger <xxyuanlinxx@hotmail.com>
2023-04-09 21:33:04 +0800

    Update NButton.js

--- FrontEnd/LostAndFound/components/reusable/Neuromorphic/Buttons/NButton.js
+++ FrontEnd/LostAndFound/components/reusable/Neuromorphic/Buttons/NButton.js
@@ -8,7 +8,23 @@ import Animated, {
 } from 'react-native-reanimated';
 import ButtonLabel from './ButtonLabel';
 
+/**
+ * A React Native button component that uses NCard and Animated from 'react-native-reanimated'.
+ */
 export default class NButton extends React.PureComponent {
+  /**
+   * Constructs a new NButton component with the specified props.
+   *
+   * @param {Object} props The props for this component.
+   * @param {Object} props.settings The settings for the NCard component.
+   * @param {string} props.label The label text for the button.
+   * @param {string} props.textColor The color of the label text.
+   * @param {string} props.fontFamily The font family for the label text.
+   * @param {function} props.onPress The callback function to be called when the button is pressed.
+   * @param {boolean} props.disabled Whether the button should be disabled or not.
+   * @param {boolean} props.blur Whether the button should have a blur effect or not.
+   * @param {boolean} props.circle Whether the button should be circular or not.
+   */
   constructor(props) {
     super(props);
     this.settings = this.props.settings;
@@ -28,6 +44,12 @@ export default class NButton extends React.PureComponent {
     };
   }
 
+
+  /**
+   * Gets the NCard component to be displayed as the button.
+   *
+   * @return {JSX.Element} The NCard component with the ButtonLabel component as its child.
+   */
   getCard() {
     let textSize = 14;
     return (
@@ -41,6 +63,9 @@ export default class NButton extends React.PureComponent {
     );
   }
 
+  /**
+   * Sets the default values for any unspecified props.
+   */
   getDefaults() {
     this.blur = this.props.blur == null ? true : this.props.blur;
     this.circle = this.props.circle == null ? false : this.props.circle;
@@ -48,6 +73,11 @@ export default class NButton extends React.PureComponent {
       this.settings.shadowRadius == null ? 4 : this.settings.shadowRadius;
   }
 
+   /**
+   * Renders the NButton component with a Pressable wrapper.
+   *
+   * @return {JSX.Element} The rendered NButton component.
+   */
   render() {
     const Card = () => this.getCard();
     console.log('button state:', this.props.disabled);
